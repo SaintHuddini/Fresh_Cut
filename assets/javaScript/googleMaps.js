da5d6072b08e3c01bf336ccdaca004c0d13fbd25
SaintHuddini <Salah1799@hotmail.com>
2020-03-11 03:00:32 +0000

    Google Maps API function added to the footer section

---
+++ assets/javaScript/googleMaps.js
@@ -0,0 +1,32 @@
+function initMap(){
+var locations = [
+      ['F.F in Stockholm',59.334591, 18.063240, 1],
+      ['F.F in Paris', 48.864716, 2.349014, 2],
+      ['F.F in London',  51.509865, -0.118092, 3]
+    ];
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 3,
+      center: new google.maps.LatLng(57.046707, 9.935932),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {  
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
+}
+  
