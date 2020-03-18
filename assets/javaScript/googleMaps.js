7f94b6b1f46eca1e828109dc12d32b40f5f893b3
SaintHuddini <Salah1799@hotmail.com>
2020-03-18 20:21:53 +0000

    new changes on the form with locations added to it and after submitting getting success or fail as result

--- assets/javaScript/googleMaps.js
+++ assets/javaScript/googleMaps.js
@@ -1,32 +1,31 @@
-function initMap(){
-var locations = [
-      ['F.F in Stockholm',59.334591, 18.063240],
-      ['F.F in Paris', 48.864716, 2.349014],
-      ['F.F in London',  51.509865, -0.118092]
+function initMap() {
+    var locations = [
+        ['F.F in Stockholm', 59.334591, 18.063240],
+        ['F.F in Paris', 48.864716, 2.349014],
+        ['F.F in London', 51.509865, -0.118092]
     ];
 
     var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 3,
-      center: new google.maps.LatLng(57.046707, 9.935932),
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+        zoom: 3,
+        center: new google.maps.LatLng(57.046707, 9.935932),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
     });
 
     var infowindow = new google.maps.InfoWindow();
 
     var marker;
 
-    for ( var i = 0; i < locations.length; i++) {  
-      marker = new google.maps.Marker({
-        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-        map: map
-      });
+    for (var i = 0; i < locations.length; i++) {
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            map: map
+        });
 
-      google.maps.event.addListener(marker, 'click', (function(marker, i) {
-        return function() {
-          infowindow.setContent(locations[i][0]);
-          infowindow.open(map, marker);
-        }
-      })(marker, i));
+        google.maps.event.addListener(marker, 'click', (function (marker, i) {
+            return function () {
+                infowindow.setContent(locations[i][0]);
+                infowindow.open(map, marker);
+            }
+        })(marker, i));
     }
 }
-  
--- assets/javaScript/sendEmail.js
+++ assets/javaScript/sendEmail.js
@@ -1,15 +1,35 @@
+/* A function that lets the user send me a email with the values of the form and then recieves a confirmation of the booking request */
+
+(function () {
+    emailjs.init("user_g4l4GLOXFxFHl2r0NkSa6");
+})();
+
+function success() {
+    $('#success').show();
+    return success()
+};
+
+function fail() {
+    $('#fail').show();
+    return fail()
+};
+
 function sendMail(contactForm) {
     emailjs.send("gmail", "fresh_fade", {
         'from_name': contactForm.name.value,
         'from_email': contactForm.emailaddress.value,
-        'booking_request': contactForm.bookingrequest.value
+        'time': contactForm.time.value,
+        'saloon': contactForm.saloon.value
     })
-    .then(
-        function(response){
-            console.log('SUCCESS', response);
-        },
-        function(error) {
-            console.log('FAILURE', error);
-        });
-        return false;
-}
+        .then(
+            function (response) {
+                console.log('SUCCESS', response);
+                return success()
+            },
+            function (error) {
+                console.log('FAILURE');
+                return fail()
+            });
+    return false;
+};
+
